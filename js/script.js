c70afe386f5dac1c4deb506458b39dfabc14bb53
Nguyentienvy <vydienkhung555@gmail.com>
2026-06-23 14:20:25 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -8,3 +8,10 @@ console.log("Chủ đề:", topic);
 console.log("Số ảnh:", imageCount);
 console.log("Đã sẵn sàng dùng JS?", isReady);
 console.log("Kiểu dữ liệu topic:", typeof topic);
+const mainTitle = document.getElementById("mainTitle");
+const welcomeText = document.getElementById("welcomeText");
+
+if (mainTitle && welcomeText) {
+  mainTitle.textContent = "Nguyễn Tiến Vỹ 👋";
+  welcomeText.textContent = "Chào mừng bạn đến với trang giới thiệu bản thân của mình!";
+}
