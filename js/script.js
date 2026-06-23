627edccd99d236a664cd9ae73335faeb661a5abf
Nguyentienvy <vydienkhung555@gmail.com>
2026-06-23 13:31:04 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
+const siteName = "MyWeb";
+let topic = "Giới thiệu bản thân";
+let imageCount = 11;
+let isReady = true;
 
+console.log("Website:", siteName);
+console.log("Chủ đề:", topic);
+console.log("Số ảnh:", imageCount);
+console.log("Đã sẵn sàng dùng JS?", isReady);
+console.log("Kiểu dữ liệu topic:", typeof topic);
